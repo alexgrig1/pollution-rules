1ffd87c9d4b2f76eafe2085a97c73466164abea4
Alex G <alexg@local>
2026-07-22 16:00:54 +0000

    Add pm10 field to ScheduledPollutionAggregate data object

---
+++ src/main/java/com/myspace/pollution/pollution_rules/ScheduledPollutionAggregate.java
@@ -0,0 +1,12 @@
+
+        @org.kie.api.definition.type.Label(value = "pm10")
+        private java.lang.Double pm10;
+        public java.lang.Double getPm10() {
+                return this.pm10;
+        }
+
+        public void setPm10(java.lang.Double pm10) {
+                this.pm10 = pm10;
+        }
+
+
