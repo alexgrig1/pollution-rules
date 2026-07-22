ef615f2631fde88813e46c29920cee74f2735802
Alex G <alexg@local>
2026-07-22 16:01:40 +0000

    Add pm25 field to ScheduledPollutionAggregate data object

--- src/main/java/com/myspace/pollution/pollution_rules/ScheduledPollutionAggregate.java
+++ src/main/java/com/myspace/pollution/pollution_rules/ScheduledPollutionAggregate.java
@@ -7,6 +7,18 @@
 
         public void setPm10(java.lang.Double pm10) {
                 this.pm10 = pm10;
+        
+        @org.kie.api.definition.type.Label(value = "pm25")
+        private java.lang.Double pm25;
+
+        public java.lang.Double getPm25() {
+                return this.pm25;
         }
+
+        public void setPm25(java.lang.Double pm25) {
+                this.pm25 = pm25;
+        }
+
+}
 
 
